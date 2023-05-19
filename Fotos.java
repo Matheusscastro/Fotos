2d970b1467958cdcc089f16b3c89eccc2c0dfddd
Matheusscastro <116644492+Matheusscastro@users.noreply.github.com>
2023-05-19 17:57:42 -0300

    Add files via upload

---
+++ Fotos.java
@@ -0,0 +1,21 @@
+package MatheusCastro;
+
+public class Fotos {
+
+	private Long idFoto;
+	private String url;
+	
+	public Long getIdFoto() {
+		return idFoto;
+	}
+	public void setIdFoto(Long idFoto) {
+		this.idFoto = idFoto;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+}
